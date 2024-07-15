d65353c6f0f8d655c7679ca1a71093e9b0493ab0
Tony Blair Maina <blairpeters13@gmail.com>
2024-07-15 14:04:34 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+const num3 = 24;
+const num4 = 5;
+let mod = Math.floor(num3 / num4);
+
+let max = Math.max(1, 3, 7, 20);
